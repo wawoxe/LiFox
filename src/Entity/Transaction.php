7e7b507ec49f492c08e37c13f4cc539c2194c865
Mykyta Melnyk <wawoxe@proton.me>
2024-02-21 22:10:44 +0100

    Added basic graphQLOperations for Transaction

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -11,13 +11,29 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GraphQl\Mutation;
+use ApiPlatform\Metadata\GraphQl\Query;
+use ApiPlatform\Metadata\GraphQl\QueryCollection;
 use App\Repository\TransactionRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: TransactionRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+    graphQlOperations: [
+        new Query(normalizationContext: ['groups' => ['query']]),
+        new QueryCollection(normalizationContext: ['groups' => ['query_collection']]),
+        new Mutation(
+            normalizationContext: ['groups' => ['query_collection']],
+            denormalizationContext: ['groups' => ['mutation']],
+            name: 'create',
+        )
+    ]
+)]
 class Transaction
 {
     public function __construct(
@@ -25,6 +41,7 @@ class Transaction
         #[ORM\Column(type: UuidType::NAME, unique: true)]
         #[ORM\GeneratedValue(strategy: 'CUSTOM')]
         #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+        #[Groups(['read', 'query', 'query_collection'])]
         public ?Uuid $id = null,
     ) {
     }
