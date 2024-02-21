1d7184fd6a022b4cb969b98a9fea9506e03fe52d
Mykyta Melnyk <wawoxe@proton.me>
2024-02-21 21:31:52 +0100

    Removed Category entity

--- migrations/Version20240221203139.php
+++ migrations/Version20240221203139.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240221202841 extends AbstractMigration
+final class Version20240221203139 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,21 +20,14 @@ final class Version20240221202841 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id UUID NOT NULL, parent_id UUID DEFAULT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_64C19C1727ACA70 ON category (parent_id)');
-        $this->addSql('COMMENT ON COLUMN category.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN category.parent_id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE transaction (id UUID NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN transaction.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1727ACA70 FOREIGN KEY (parent_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE category DROP CONSTRAINT FK_64C19C1727ACA70');
-        $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE transaction');
     }
 }
